a8a335589ee190396f0436c9ea6d1761334e3005
Jad Pamment <jad.pamment@assurity.co.nz>
2017-03-03 09:44:53 +1300

    Updating Gradle

--- main/gameModule/tests/controllertest.py
+++ main/gameModule/tests/controllertest.py
@@ -1,4 +1,4 @@
 """This module contains controller tests. Stop asking stupid questions."""
 
 import pytest
-from gameModule.controller import KeyboardController
+from controller import KeyboardController
